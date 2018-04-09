9a667e0dcbc27ce2b81c9cd94c92ab1a22d1ad9b
Alan Cardoso <alanscardoso@gmail.com>
2018-04-09 11:26:35 -0300

    adding commentaries

--- client.py
+++ client.py
@@ -7,11 +7,13 @@ import argparse
 MAX_PACKET = 2048
 
 def is_url_ok(url):
+    """ Verifica se a url solicitada possui a formatacao correta. A url pode ou nao conter o protocolo e porta."""
     protocol = ''
     host = ''
     port = ''
     file = ''
 
+    # verifica se existe protocolo discriminado
     list = url.split('://')
     if len(list) > 1:
         protocol = list[0]
@@ -19,6 +21,7 @@ def is_url_ok(url):
     else:
         host = list[0]
 
+    # verifica se existe algum arquivo discriminado
     list = host.split('/')
     if len(list) > 1:
         print(list)
@@ -28,6 +31,7 @@ def is_url_ok(url):
             print(i)
             file += '/' + list[i]
 
+    # verifica se existe porta discriminada
     list = host.split(':')
     if len(list) > 1:
         host = list[0]
@@ -36,9 +40,12 @@ def is_url_ok(url):
         host = list[0]
         port = '80'
 
+    # recupera o ip do host
+    host = socket.gethostbyname(host)
     if file != '':
         host += file
 
+    # somente o protocolo HTTP e suportado
     if protocol != '' and protocol != 'http':
         print('Protocolo nao suportado')
         return False, host, port
@@ -46,6 +53,7 @@ def is_url_ok(url):
     return True, host, port
 
 def write_file(file_name, content):
+    """ grava arquivo de saida """
     html = ''
     for i in range(1,len(content)):
         html += content[i]
@@ -58,11 +66,13 @@ def write_file(file_name, content):
     file.close()
 
 def handle_server(server):
+    """ recebe os dados do servidor """
     total_data = [];
     data = '';
     timeout = 2
     begin = time.time()
 
+    # mantem um loop aberto por tempo limitado para recuperar todos os dados
     while True:
         if time.time() - begin > timeout:
             break
@@ -83,23 +93,24 @@ def handle_server(server):
     
 
 def main():
+    # valida argumentos
     parser = argparse.ArgumentParser()
     parser.add_argument('url')
     parser.add_argument('file_name')
     args = parser.parse_args()
 
+    # valida se a url inserida segue os padrões
     is_ok, host, port = is_url_ok(args.url)
-
     if not is_ok:
         return
 
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.connect((socket.gethostbyname(host), int(port)))
-
+    # configuracoes do socket e do request
     request = "GET / HTTP/1.1\nHost: {0}:{1}\nConnection: close\n\n".format(host, port).encode('utf8')
-
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.connect((host, int(port)))
     server.send(request)
 
+    # recupera dados da pagina e salva em um arquivo
     content = handle_server(server)
     write_file(args.file_name, content)
 
