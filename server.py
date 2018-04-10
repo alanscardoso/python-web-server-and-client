3a8fbab1d57f3a40a8db4b68146c30360ce27864
Alan Cardoso <alanscardoso@gmail.com>
2018-04-09 23:45:36 -0300

    minor adjustments

--- server.py
+++ server.py
@@ -1,10 +1,11 @@
 import os
 import socket
 import argparse
+import mimetypes
 
 MAX_PACKET = 2048
 
-def handle_client(client):
+def handle_client(client, folder):
     """ recebe os dados do client e retorna os devidos status e informacoes solicitadas """
     request = client.recv(MAX_PACKET)
     request = request.decode('utf8')
@@ -54,7 +55,7 @@ def handle_client(client):
     
     # se tudo estiver certo com o request retorna 200(OK)
     if is_request_ok:
-        files = os.listdir()
+        files = os.listdir(folder)
 
         # se nao ha arquivo discriminado no get, procura pelo index.html
         if file == '/':
@@ -63,7 +64,7 @@ def handle_client(client):
                 status = 'HTTP/1.1 200 OK\n'
                 contet_type = 'Content-Type: text/html\n'
 
-                response = open('index.html', 'r').read().encode('utf8')
+                response = open(folder + 'index.html', 'r').read().encode('utf8')
             # se nao existir o index.html, retorna a listagem de arquivos do diretorio
             else:
                 status = 'HTTP/1.1 200 OK\n'
@@ -74,37 +75,43 @@ def handle_client(client):
                 for file in files:
                     # so adiciona arquivos que nao sejam invisiveis na listagem
                     if file[0] != '.':
-                        files_str +="<a href=/{0}><li>{0}</li></a>".format(file)
+                        size = os.path.getsize(folder+file)
+                        files_str += """<tr>
+                                            <td><a href=/{0}><li>{1}</li></a></td>
+                                            <td>{2} bytes</td>
+                                          </tr>""".format(file, file, size)
+
 
                 response = """
                     <!DOCTYPE html>
                     <html lang='pt-br'>
                     <meta charset="UTF-8">
                     <body>
-                    <ul>
-                    {0}
-                    </ul>
-                    </body>
-                    </html>
-                """.format(files_str).encode('utf8')
+                    <h2>Listing Files</h2>
+                    <table style="width:40%">
+                        {0}
+                        </table>
+                        </body>
+                        </html>""".format(files_str).encode('utf8')
         # se houver arquivo discriminado no request, realiza o tratamento adequado
         else:
             file = file[1:]
             # verifica se o arquivo solicitado existe no diretorio
-            if file in files:
+            if file in files:        
                 extension = file.split('.')[1]
-                # se for imagem ou pdf le o arquivo no modo binario
-                if extension in ['png','jpg','gif','ico','pdf']:
+                # se for text le o arquivo normalmente
+                if extension in ['css','htm','html','txt']:
                     status = 'HTTP/1.1 200 OK\n'
                     contet_type = 'Content-Type: text/html\n'
 
-                    response = open(file, 'rb').read() # rb = read binary
-                # se nao le o arquivo no modo texto
+                    response = open(folder + file, 'r').read().encode('utf8')
+                # se nao le o arquivo no modo binario
                 else:
                     status = 'HTTP/1.1 200 OK\n'
-                    contet_type = 'Content-Type: text/html\n'
+                    contet_type = 'Content-Type: {0}\n'.format(mimetypes.guess_type(folder + file)[0])
+                    print(mimetypes.guess_type(folder + file)[0])
 
-                    response = open(file, 'r').read().encode('utf8')
+                    response = open(folder + file, 'rb').read() # rb = read binary
             # se o arquivo solicitado nao existir no diretorio, retorna 404(not found)
             else:
                 status = 'HTTP/1.1 404 ERROR\n'
@@ -116,7 +123,7 @@ def handle_client(client):
                     <meta charset="UTF-8">
                     <body>
                     <h1>404 file not found</h1>
-                    O arquivo {0} não existe não existe no servidor.
+                    O arquivo {0} não existe no servidor.
                     </body>
                     </html>
                 """.format(file).encode('utf8')
@@ -134,20 +141,27 @@ def handle_client(client):
 def main():
     #valida argumentos
     parser = argparse.ArgumentParser()
-    parser.add_argument('host')
     parser.add_argument('port')
+    parser.add_argument('folder')
     args = parser.parse_args()
+
+    # verifica se existe folder e se está correta
+    folder = ''
+    if args.folder[-1] != '/':
+        folder = args.folder + '/'
+    else:
+        folder = args.folder
 
     # configuracoes do socket
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind((args.host, int(args.port)))
+    server.bind(('', int(args.port)))
     server.listen(1) # permite apenas uma conexao por vez, esse parametro pode ser aumentado
 	
     print('Server running...')
     
     while True:
         client, address = server.accept()
-        handle_client(client)
+        handle_client(client, folder)
         
 if __name__ == '__main__':
     main()
